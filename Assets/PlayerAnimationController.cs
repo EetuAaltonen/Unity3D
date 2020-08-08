70fe4d3e83ce7a70325ad7d1145cbb7ff96da14d
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2020-08-09 00:21:11 +0300

    Update Player model. Add new Player animation controller. Remove old Player test models.

---
+++ Assets/PlayerAnimationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    public Animator Animator;
+    private PlayerMovement _movementScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _movementScript = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movementSpeed = _movementScript.GetMovementSpeed();
+        if (Animator != null)
+        {
+            Animator.SetFloat("MovementSpeed", movementSpeed);
+        }
+    }
+}
