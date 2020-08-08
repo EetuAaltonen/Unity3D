adbde144f30f4ff8adcb95a32d3ad871459ebf09
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2020-08-09 00:28:09 +0300

    Tweak Player movement logic.

--- Assets/Characters/Player/Scripts/PlayerGravity.cs
+++ Assets/Characters/Player/Scripts/PlayerGravity.cs
@@ -7,23 +7,30 @@ public class PlayerGravity : MonoBehaviour
     public CharacterController Controller;
 
     public float Gravity = -9.81f;
+    private float GravityScale = 2f;
     public bool IsGrounded;
 
     public Transform GroundCheck;
     public float GroundDistance = 0.4f;
     public LayerMask GroundMask;
-    public Vector3 Velocity;
+    
+    private Vector3 _velocity;
 
     // Update is called once per frame
     void Update()
     {
         IsGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
 
-        if (IsGrounded && Velocity.y < 0) {
-            Velocity.y = -2f;
+        if (IsGrounded && _velocity.y < 0) {
+            _velocity.y = -2f;
         }
 
-        Velocity.y += Gravity * Time.deltaTime;
-        Controller.Move(Velocity * Time.deltaTime);
+        _velocity.y += Gravity * GravityScale * Time.deltaTime;
+        Controller.Move(_velocity * Time.deltaTime);
+    }
+
+    public void SetVelocity(float velocity)
+    {
+        _velocity.y = velocity;
     }
 }
--- Assets/Characters/Player/Scripts/PlayerMovement.cs
+++ Assets/Characters/Player/Scripts/PlayerMovement.cs
@@ -4,24 +4,89 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController Controller;
+    public bool IsRunning;
+    public float MaxRunSpeed;
+    public float MaxWalkSpeed;
+    public float JumpVelocity;
 
-    public float Speed = 12f;
+    [SerializeField] private GameObject _playerRef;
 
-    GameObject playerRef;
-    ChangePOV povScript;
+    private CharacterController _controller;
+    private PlayerGravity _gravityScript;
+    private PlayerCamera _cameraScript;
+    private ChangePOV _povScript;
+
+    private Camera _thirdPersonCamera;
+    private float _maxSpeed;
+    private float _smoothSpeed;
+
+    private float _turnSmoothTime = 0.2f;
+    private float _turnSmoothVelocity;
+
 
     void Start() {
-        playerRef = GameObject.Find("Player");
-        povScript = playerRef.GetComponent<ChangePOV>();
+        _controller = GetComponent<CharacterController>();
+        _gravityScript = GetComponent<PlayerGravity>();
+        _cameraScript = GetComponent<PlayerCamera>();
+        _povScript = GetComponent<ChangePOV>();
+
+        _thirdPersonCamera = _cameraScript.ThirdPersonCamera;
+
+        StopMovement();
     }
 
     // Update is called once per frame
     void Update() {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
-
+        var isFirstPerson = _povScript.IsFirstPerson();
         Vector3 move = transform.right * x + transform.forward * z;
-        Controller.Move(move * Speed * Time.deltaTime);
+
+        if (move.magnitude > 0)
+        {
+            if (!isFirstPerson)
+            {
+                float targetAngle = _thirdPersonCamera.transform.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            }
+            _smoothSpeed = Mathf.Lerp(_smoothSpeed, _maxSpeed, Time.deltaTime);
+        }
+        else
+        {
+            StopRunning();
+            _smoothSpeed = Mathf.Lerp(_smoothSpeed, 0, Time.deltaTime);
+        }
+        _smoothSpeed = Mathf.Clamp(_smoothSpeed, 0, _maxSpeed);
+        _controller.Move(move * _smoothSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && _gravityScript.IsGrounded)
+        {
+            _gravityScript.SetVelocity(Mathf.Sqrt(JumpVelocity * -1f * _gravityScript.Gravity));
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            IsRunning = !IsRunning;
+            _maxSpeed = IsRunning ? MaxRunSpeed : MaxWalkSpeed;
+        }
+    }
+
+    public float GetMovementSpeed()
+    {
+        return _smoothSpeed;
+    }
+
+    public void StopMovement()
+    {
+        StopRunning();
+        transform.up = Vector3.zero;
+        _smoothSpeed = 0;
+    }
+
+    public void StopRunning()
+    {
+        IsRunning = false;
+        _maxSpeed = MaxWalkSpeed;
     }
 }
