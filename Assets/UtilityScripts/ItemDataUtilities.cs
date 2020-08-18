943262427c1ce90b35db54c2995a3201b5045b85
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2020-08-18 14:59:43 +0300

    Add utility scripts for ItemData.

---
+++ Assets/UtilityScripts/ItemDataUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ItemDataUtilities
+{
+    public static bool CompareItems(ItemData item1, ItemData item2)
+    {
+        if (item1.Id != item2.Id) return false;
+        if (item1.Name != item2.Name) return false;
+        if (item1.Type != item2.Type) return false;
+
+        if (item1.ItemBuffs.Count != item2.ItemBuffs.Count) return false;
+        for (int i = 0; i < item1.ItemBuffs.Count; i++)
+        {
+            if (!CompareItemBuffs(item1.ItemBuffs[i], item2.ItemBuffs[i])) return false;
+        }
+        return true;
+    }
+
+    public static bool CompareItemBuffs(ItemBuff buff1, ItemBuff buff2)
+    {
+        if (buff1.Type != buff2.Type) return false;
+        if (buff1.Amount != buff2.Amount) return false;
+        if (buff1.Durability != buff2.Durability) return false;
+        return true;
+    }
+}
