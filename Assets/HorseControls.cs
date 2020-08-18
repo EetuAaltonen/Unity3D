cf259fcbf5bf02e9df7d19710d757a17956133f5
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2020-08-18 15:36:55 +0300

    Add logic for controlling the Horse.

---
+++ Assets/HorseControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseControls : MonoBehaviour
+{
+    private CharacterController _controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("ArrowHorizontal");
+        float z = Input.GetAxisRaw("ArrowVertical");
+        float movementSpeed = 5f;
+        Vector3 move = transform.right * x + transform.forward * z;
+        if (z != 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), Time.deltaTime);
+            _controller.Move(transform.forward * movementSpeed * Time.deltaTime);
+        }
+    }
+}
