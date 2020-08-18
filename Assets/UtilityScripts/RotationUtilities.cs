535fcd26307b1d3be5d2d70bc19ebc774b55c6d8
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2020-08-18 15:00:34 +0300

    Add utility scripts for rotation calculations.

---
+++ Assets/UtilityScripts/RotationUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RotationUtilities
+{
+    // Start is called before the first frame update
+    public static float ScaleRotation(float angle)
+    {
+        return angle > 180f ? angle - 360f : angle;
+    }
+}
