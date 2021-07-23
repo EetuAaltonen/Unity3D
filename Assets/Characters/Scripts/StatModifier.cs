f59df486d63f5fa54fd9fbb8e68bcaee345d0770
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2021-07-23 15:49:24 +0300

    Add class for StatModifier.

---
+++ Assets/Characters/Scripts/StatModifier.cs
@@ -0,0 +1,19 @@
+﻿[System.Serializable]
+public class StatModifier
+{
+    public StatModifierType Type;
+    public float Amount;
+    public StatModifierValueType ValueType;
+    public float Durability;
+    public StatModifierTarget Target;
+    public string Source;
+    public StatModifier(StatModifierType type, float amount, StatModifierValueType valueType, float durability, StatModifierTarget target)
+    {
+        Type = type;
+        Amount = amount;
+        ValueType = valueType;
+        Durability = durability;
+        Target = target;
+        Source = null;
+    }
+}
