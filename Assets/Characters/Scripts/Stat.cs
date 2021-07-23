279c12e8485f9b6707fa75b5fffcfe31ace3c1dc
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2021-07-23 15:47:57 +0300

    Add class for Stat.

---
+++ Assets/Characters/Scripts/Stat.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat
+{
+    [SerializeField]
+    private float baseValue;
+
+    private List<StatModifier> modifiers = new List<StatModifier>();
+
+    public float GetValue()
+    {
+        float modifiedValue = baseValue;
+        modifiers.ForEach(x => modifiedValue += x.Amount);
+        return modifiedValue;
+    }
+
+    public void AddModifier(StatModifier modifier)
+    {
+        if (modifier.Amount != 0)
+        {
+            modifiers.Add(modifier);
+        }
+    }
+
+    public void RemoveModifier(StatModifier modifier)
+    {
+        if (modifier.Amount != 0)
+        {
+            modifiers.Remove(modifier);
+        }
+    }
+}
