1e10fac7055ff1060bdf6394de6327994b60fa2a
TheHeroOfNoOne <eetu.aaltonen98@gmail.com>
2021-07-26 21:11:07 +0300

    Add prefab for ItemInstance and variant prefab for ItemInstanceBattleaxe. Rename weapon model assets to use 'Model' suffix.

---
+++ Assets/Items/Scripts/ItemInstance.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInstance : MonoBehaviour
+{
+    public ItemData Item;
+    public int Amount;
+}
